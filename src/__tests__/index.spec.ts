dbf729f35a9ff1b4dddb8d7e99d4364fe96ad5c4
Umidbek Karimov <uma.karimov@gmail.com>
2020-03-27 10:32:15 +0500

    feat: `ArrayProp`, `BooleanProp`, `DateProp`, and `ObjectProp`.

--- src/__tests__/index.spec.ts
+++ src/__tests__/index.spec.ts
@@ -3,7 +3,11 @@ import * as api from '..';
 test('public api', () => {
   expect(api).toMatchInlineSnapshot(`
     Object {
+      "ArrayProp": [Function],
+      "BooleanProp": [Function],
+      "DateProp": [Function],
       "NumberProp": [Function],
+      "ObjectProp": [Function],
       "Prop": [Function],
       "StringProp": [Function],
       "parseDTO": [Function],
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
 export * from './parseDTO';
 export * from './serializeDTO';
-export * from './props/Prop';
+
+export * from './props/ArrayProp';
+export * from './props/BooleanProp';
+export * from './props/DateProp';
 export * from './props/NumberProp';
+export * from './props/ObjectProp';
+export * from './props/Prop';
 export * from './props/StringProp';
